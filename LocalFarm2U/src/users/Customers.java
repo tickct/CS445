b286d2c32afce4e015d8f35507aaea3ea6d44e11
Sean Gray <sgray2@hawk.iit.edu>
2016-10-04 19:11:00 -0500

    switched to singular tense

--- LocalFarm2U/src/users/Customers.java
+++
@@ -1,9 +0,0 @@
-package users;
-
-public class Customers extends User{
-
-	public Customers(String nm, String pN, String ad, String zipCode) {
-		super(nm, pN, ad, zipCode);
-		// TODO Auto-generated constructor stub
-	}
-}
