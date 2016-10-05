5fa6c9c9b486d2fccb65186f9cf652f456cd03a6
Sean Gray <sgray2@hawk.iit.edu>
2016-10-04 19:16:01 -0500

    refratored to be more generic

--- LocalFarm2U/src/register/UserRegistration.java
+++ LocalFarm2U/src/register/UserRegistration.java
@@ -1,5 +1,26 @@
 package register;
 
-public class UserRegistration {
+import java.util.ArrayList;
 
+import users.User;
+
+public class UserRegistration {
+	ArrayList<User> userList;
+	public UserRegistration(){
+		userList = new ArrayList<User>();
+	}
+	public int getSize(){
+		return userList.size();
+	}
+	public void addUser(User user){
+		userList.add(user);
+	}
+	public void removeUser(User user){
+		if(userList.contains(user)){
+			userList.remove(user);
+		}
+		else{
+			throw new IllegalArgumentException();
+		}
+	}
 }
--- LocalFarm2U/src/tests/register/RegisterTest.java
+++ LocalFarm2U/src/tests/register/RegisterTest.java
@@ -4,29 +4,29 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
-import register.FarmerRegistration;
+import register.UserRegistration;
 import users.Farmer;
 
 public class RegisterTest {
-		FarmerRegistration list=new FarmerRegistration();
+		UserRegistration list=new UserRegistration();
 		Farmer farmer=new Farmer("Sean Gray","1231231234","123ABC","123456");
 		Farmer farmer2=new Farmer("Sean Gray","1231231234","123ABC","123456");
 		
 	@Test
 	public void AddFarmerAddstoList(){
-		list.addFarmer(farmer);
+		list.addUser(farmer);
 		assertEquals(list.getSize(),1);
-		list.addFarmer(farmer2);
+		list.addUser(farmer2);
 		assertEquals(list.getSize(),2);
 	}
 	@Test
 	public void RevoveFarmerRemoves(){
-		list.addFarmer(farmer);
-		list.removeFarmer(farmer);
+		list.addUser(farmer);
+		list.removeUser(farmer);
 		assertEquals(list.getSize(),0);
 	}
 	@Test (expected=IllegalArgumentException.class)
 	public void ThrowErrorWhenFramerDosntExist(){
-		list.removeFarmer(farmer2);
+		list.removeUser(farmer2);
 	}
 }
