970271dcb1a9979848e87197a1bfa68e04d8b46e
Sean Gray <sgray2@hawk.iit.edu>
2016-10-11 12:14:10 -0500

    updated for new user fields

--- LocalFarm2U/src/users/Customer.java
+++ LocalFarm2U/src/users/Customer.java
@@ -2,8 +2,7 @@ package users;
 
 public class Customer extends User{
 
-	public Customer(String nm, String pN, String ad, String zipCode) {
-		super(nm, pN, ad, zipCode);
-		// TODO Auto-generated constructor stub
+	public Customer(String nm, String pN, String ad, String zipCode,String user,String pass) {
+		super(nm, pN, ad, zipCode, user, pass);
 	}
 }
--- LocalFarm2U/src/users/Farmer.java
+++ LocalFarm2U/src/users/Farmer.java
@@ -3,9 +3,8 @@ package users;
 
 public class Farmer extends User{
 
-	public Farmer(String nm, String pN, String ad, String zipCode) {
-		super(nm, pN, ad, zipCode);
-		// TODO Auto-generated constructor stub
+	public Farmer(String nm, String pN, String ad, String zipCode,String user,String pass) {
+		super(nm, pN, ad, zipCode,user,pass);
 	}
 
 }
